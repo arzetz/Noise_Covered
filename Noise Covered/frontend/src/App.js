36e9d725a51eff860ec2562f0e8848745a3d48d4
freeny-afk <artemkach@inbox.ru>
2024-09-04 06:34:25 +0300

    first front step

---
+++ Noise Covered/frontend/src/App.js
@@ -0,0 +1,14 @@
+import HomePage from './components/HomePage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+     <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ Noise Covered/frontend/src/components/HomePage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./HomePage.css"; // Импортируйте файл стилей
+import { Container, Row, Col, Nav } from "react-bootstrap";
+
+function HomePage() {
+  return (
+    <div className="background-container">
+      <Container className="centered-container main-header">
+        <Row>
+          <Col lg={12} className="main-header">
+            <span className="main">NOISE|COVE<span className="RED">RED</span></span>
+          </Col>
+          </Row>
+      </Container>
+      <Container
+        className="centered-container"
+      >
+        <Row>
+          <Col className="my-nav">
+            <Nav>
+              <Nav.Item className="mx-5">
+                <Nav.Link href="#"><span className="menu">ГЛАВНАЯ</span></Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="mx-5">
+                <Nav.Link href="#"><span className="menu">О НАС</span></Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="mx-5">
+                <Nav.Link href="#"><span className="menu">КАТАЛОГ</span></Nav.Link>
+              </Nav.Item>
+              <Nav.Item className="mx-5">
+                <Nav.Link href="#"><span className="menu">КОНТАКТЫ</span></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default HomePage;
